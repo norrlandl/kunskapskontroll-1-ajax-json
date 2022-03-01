26f42a2146d6b03d2c66f3ff143ba6d542324c58
norrlandl <lina_holmlund@hotmail.com>
2022-03-01 11:11:34 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ const btn = document.getElementById('button');
 const input = document.getElementById('input');
 const result = document.getElementById('result');
 const resultRosters = document.getElementById('result-rosters');
-
 const resultBtn = document.getElementById('button-rosters');
 
 /**
@@ -22,19 +21,20 @@ const resultBtn = document.getElementById('button-rosters');
 btn.addEventListener('click', fetchDataTeam);
 resultBtn.addEventListener('click', fetchDataRoster);
 
-// $('#result #button-rosters').on('click', fetchDataRoster);
+/**
+ *
+ *
+ *
+ */
 
 async function fetchDataTeam(e) {
   resultRosters.innerHTML = '';
-  // e.preventDefault();
-  // const theClickedLink = e.target;
-  // const subject = theClickedLink.id;
-  // const title = theClickedLink.innerText;
 
   try {
     const response = await fetch(
       'https://statsapi.web.nhl.com/api/v1/teams/' + input.value
     );
+
     // if (!response.ok) {
     //   throw new Error('Something went wrong with the server');
     // }
@@ -42,18 +42,14 @@ async function fetchDataTeam(e) {
     console.log(data);
     console.log(data.teams);
 
-    // const dataRosters = await responseRosters.json();
-    // console.log(dataRosters);
-
     let nhl = '';
     for (let teams of data.teams) {
       nhl += `
-                <i>${teams.teamName}</i>
-                <h5>${teams.name}</h5>
+                <i> ${teams.teamName} </i>
+                <h5> ${teams.name} </h5>
                 <h6> City: ${teams.locationName}</h6>
                 <h6> Conference: ${teams.conference.name}</h6>
                 <h6> Webbsite:  <a href="${teams.officialSiteUrl}" target="_blank">${teams.officialSiteUrl}</a></h6>
-            
             `;
     }
     result.innerHTML = nhl;
@@ -69,24 +65,27 @@ async function fetchDataRoster(e) {
         input.value +
         '?expand=team.roster'
     );
-    const dataRosters = await responseRosters.json();
-    console.log(dataRosters);
 
+    const dataRosters = await responseRosters.json();
+    // console.log(dataRosters);
     let teamRosters = dataRosters.teams[0].roster.roster;
-    console.log(teamRosters);
+    // console.log(teamRosters);
 
     let rosters = '';
     for (let player of teamRosters) {
       rosters += `
                   <div class="player">
-                  <h6>${player.jerseyNumber}</h6>
-
+                  <div class="number">${player.jerseyNumber}</div>
                   <i>${player.position.name}</i>
-                  <h5 class="name">${player.person.fullName}</h5>
+                  <h5 class="name"><a id="roster-info" href="#">${player.person.fullName}</a></h5>
                   </div>`;
     }
+
     resultRosters.innerHTML = rosters;
   } catch (error) {
     console.log(error);
   }
 }
+
+// // Player ID
+// `https://statsapi.web.nhl.com/api/v1/people/${player.person.id}`;
---
+++ temp.js
@@ -0,0 +1,23 @@
+// let theTeams = '';
+// for (let i = 0; i < data.teams.length; i++) {
+//   theTeams = data.teams[i].name;
+//   console.log(theTeams);
+
+//   if (input.value === theTeams) {
+//     console.log(theTeams);
+//   }
+// }
+
+// $('#result #button-rosters').on('click', fetchDataRoster);
+
+
+
+// const responseRostersID = await fetch(
+//   `https://statsapi.web.nhl.com/api/v1/people/`
+// );
+// const dataRostersID = await responseRostersID.json();
+// console.log(dataRostersID);
+
+
+https://statsapi.web.nhl.com/api/v1/people/${player.person.id}
+
